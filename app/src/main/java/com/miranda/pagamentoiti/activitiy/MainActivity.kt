fc6d3a12fef5176c414685eba37d8d7e2aad6369
unknown <sonardevplus@gmail.com>
2022-01-10 20:21:42 -0300

    Primeiro commit

---
+++ app/src/main/java/com/miranda/pagamentoiti/activitiy/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.miranda.pagamentoiti.activitiy
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.miranda.pagamentoiti.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ app/src/main/java/com/miranda/pagamentoiti/activitiy/PaymentDoneActivity.kt
@@ -0,0 +1,17 @@
+package com.miranda.pagamentoiti.activitiy
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.PersistableBundle
+import android.widget.TextView
+import com.miranda.pagamentoiti.R
+
+class PaymentDoneActivity : AppCompatActivity(R.layout.activity_payment_done){
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        //val amountTextView = findViewById<TextView>(R.id.payment_done_divide_amount_text)
+
+        getS
+    }
+}
